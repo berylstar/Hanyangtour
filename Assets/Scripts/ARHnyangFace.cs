f8784dfcf1fb7b7953cc41521b069ced4c0f24b4
berylstar <minsang0610@naver.com>
2022-05-27 12:18:46 +0900

    정리

--- Assets/Scripts/ARHnyangFace.cs
+++ Assets/Scripts/ARHnyangFace.cs
@@ -2,22 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARCore;
+using Unity.Collections;
 
 public class ARHnyangFace : MonoBehaviour
 {
-    ARFaceManager arFaceManager;
+    public GameObject nosePrefab;
 
-    public Material faceMaterial;
+    ARFaceManager arFaceManager;
+    ARSessionOrigin sessionOrigin;
+
+    NativeArray<ARCoreFaceRegionData> faceRegions;
+
+    GameObject noseObject;
 
     void Start()
     {
         arFaceManager = GetComponent<ARFaceManager>();
-        arFaceManager.facePrefab.GetComponent<MeshRenderer>().material = faceMaterial;
+        sessionOrigin = GetComponent<ARSessionOrigin>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ARCoreFaceSubsystem subsystem = (ARCoreFaceSubsystem)arFaceManager.subsystem;
+
+        foreach(ARFace face in arFaceManager.trackables)
+        {
+            subsystem.GetRegionPoses(face.trackableId, Allocator.Persistent, ref faceRegions);
+
+            foreach (ARCoreFaceRegionData faceRegion in faceRegions)
+            {
+                ARCoreFaceRegion regionType = faceRegion.region;
+
+                if(regionType == ARCoreFaceRegion.NoseTip)
+                {
+                    if(!noseObject)
+                    {
+                        noseObject = Instantiate(nosePrefab, sessionOrigin.trackablesParent);
+                    }
+
+                    noseObject.transform.localPosition = faceRegion.pose.position;
+                    noseObject.transform.localRotation = faceRegion.pose.rotation;
+                }
+            }
+        }
+    }
+
+
+    public void CaptureScreenForMobile(string fileName)
+    {
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
+
+        // do something with texture
+        string albumName = "HanyangTOUR";
+
+        NativeGallery.SaveImageToGallery(texture, albumName, fileName, (success, path) =>
+        {
+            Debug.Log(success);
+            Debug.Log(path);
+        });
+
+        // cleanup
+        Object.Destroy(texture);
     }
 }
